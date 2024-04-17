29847a16030deccc788579d6b6525ae30603dfed
Necdet UYGUR <necdet.uygur@gmail.com>
2024-04-17 17:45:12 +0300

    VirtualBox

--- virtualbox.sh
+++ virtualbox.sh
@@ -8,3 +8,7 @@ sudo apt-get update -y
 
 curl -L -o "virtualbox.deb" -C - "https://download.virtualbox.org/virtualbox/7.0.16/virtualbox-7.0_7.0.16-162802~Ubuntu~jammy_amd64.deb"
 sudo apt install -f -y ./virtualbox.deb
+
+# sudo apt install --reinstall virtualbox-dkms -y
+# sudo apt install libelf-dev -y
+# sudo /sbin/vboxconfig
