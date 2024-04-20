a7845b128dc59bc9113b082afd00acdd07e6663f
Necdet UYGUR <necdet.uygur@gmail.com>
2024-04-20 22:49:25 +0300

    Cursor

--- cursor.sh
+++ cursor.sh
@@ -1,4 +1,11 @@
 sudo echo $(date "+%F %H:%M:%S")
+
 sudo apt update -y
 sudo apt install gnome-tweaks -y
-git clone https://github.com/necdetuygur/el-capitan-cursor ~/.icons/El\ Capitan\ -\ Cursor
+
+# git clone https://github.com/necdetuygur/el-capitan-cursor ~/.icons/El\ Capitan\ -\ Cursor
+
+curl -L -o "macOS-Monterey.tar.gz" -C - "https://github.com/ful1e5/apple_cursor/releases/download/v2.0.0/macOS-Monterey.tar.gz"
+tar -xvf macOS-Monterey.tar.gz
+# mv macOS-Monterey ~/.icons/
+sudo mv macOS-Monterey /usr/share/icons/
