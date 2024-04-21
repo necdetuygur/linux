9b441302ddf4da126cc5453141f5343070e500fe
Necdet UYGUR <necdet.uygur@gmail.com>
2024-04-21 18:26:24 +0300

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,3 +1,7 @@
 sudo echo $(date "+%F %H:%M:%S")
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
+
+# sudo docker run -it --rm -d -p 8080:80 --name web1 nginx:alpine
+# sudo docker ps
+# sudo docker exec -ti web1 sh
