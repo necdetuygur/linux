dcfc936bef13679e16f67299f89804b50b922ebf
Necdet UYGUR <necdet.uygur@gmail.com>
2024-04-23 14:57:39 +0300

    Font

--- font.sh
+++ font.sh
@@ -4,7 +4,8 @@ sudo apt update -y
 sudo apt install unzip -y
 sudo apt install font-manager -y
 
-curl -L -o "fira-sans.zip" -C - "https://www.fontsquirrel.com/fonts/download/fira-sans"
+# curl -L -o "fira-sans.zip" -C - "https://www.fontsquirrel.com/fonts/download/fira-sans"
+curl -L -o "fira-sans.zip" -C - "https://www.1001fonts.com/download/fira-sans.zip"
 unzip fira-sans.zip -d fira-sans
-open fira-sans
+open fira-sans &
 font-manager &
