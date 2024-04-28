def56635e7381c9e16c81e74983ab1f1fd8a8886
Necdet UYGUR <necdet.uygur@gmail.com>
2024-04-29 01:03:37 +0300

    yt-dlp

--- yt-dlp.sh
+++ yt-dlp.sh
@@ -11,6 +11,11 @@ sudo echo ""
 sudo apt update -y
 sudo apt install python3-pip -y
 sudo pip3 install yt-dlp
+# yt-dlp --version
+
+curl -L -o "yt-dlp" -C - "https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp"
+sudo mv yt-dlp /usr/local/bin/
+
 yt-dlp --version
 
 # yt-dlp "YT_ID" -f mp4 -S res:480
