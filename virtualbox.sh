718099b58ae1ac647145c6ef59bdde8ee926a09f
Necdet UYGUR <necdet.uygur@gmail.com>
2024-04-29 11:42:54 +0300

    VirtualBox

--- virtualbox.sh
+++ virtualbox.sh
@@ -8,11 +8,11 @@ sudo echo ""
 sudo echo ""
 sudo echo ""
 
-sudo add-apt-repository universe
-sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) universe"
-sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main universe restricted multiverse"
-sudo add-apt-repository "deb http://archive.canonical.com/ubuntu $(lsb_release -sc) partner"
-sudo apt-get update -y
+# sudo add-apt-repository universe
+# sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) universe"
+# sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main universe restricted multiverse"
+# sudo add-apt-repository "deb http://archive.canonical.com/ubuntu $(lsb_release -sc) partner"
+# sudo apt-get update -y
 
 curl -L -o "virtualbox.deb" -C - "https://download.virtualbox.org/virtualbox/7.0.16/virtualbox-7.0_7.0.16-162802~Ubuntu~jammy_amd64.deb"
 sudo apt install -f -y ./virtualbox.deb
