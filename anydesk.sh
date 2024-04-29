e65833cfc512a389eaf4885af559803bd8381cd4
Necdet UYGUR <necdet.uygur@gmail.com>
2024-04-29 11:39:41 +0300

    AnyDesk

--- anydesk.sh
+++ anydesk.sh
@@ -8,7 +8,7 @@ sudo echo ""
 sudo echo ""
 sudo echo ""
 
-curl -L -o "anydesk.deb" -C - "https://download.anydesk.com/linux/anydesk_6.3.1-1_amd64.deb"
+curl -L -o "anydesk.deb" -C - "https://download.anydesk.com/linux/anydesk_6.3.2-1_amd64.deb"
 sudo apt install -y ./anydesk.deb
 sudo apt install -f
 sudo apt install -y ./anydesk.deb
