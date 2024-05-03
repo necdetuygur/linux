c5c6a725974c516102110f561f2cd4fd4e8b2c9e
Necdet UYGUR <necdet.uygur@gmail.com>
2024-05-04 00:31:42 +0300

    .NET

--- dotnet.sh
+++ dotnet.sh
@@ -7,3 +7,6 @@ sudo echo ""
 wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
 chmod +x ./dotnet-install.sh
 ./dotnet-install.sh --version latest
+
+echo "export PATH=\"$PATH:$HOME/.dotnet\"" >> ~/.profile
+echo "export PATH=\"$PATH:$HOME/.dotnet\"" >> ~/.bashrc
