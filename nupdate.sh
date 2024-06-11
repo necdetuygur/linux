55d76a43467f0c312b128d839793d7976b887b42
Necdet UYGUR <necdet.uygur@gmail.com>
2024-06-11 18:02:52 +0300

    nUpdate

--- nupdate.sh
+++ nupdate.sh
@@ -1,3 +1,9 @@
+sudo echo ""
+sudo echo "   /*********************\\"
+sudo echo "   | $(date "+%F %H:%M:%S") |"
+sudo echo "   \\*********************/"
+sudo echo ""
+
 sudo cat << EOF > /usr/local/bin/nupdate
 #!/bin/bash
 
