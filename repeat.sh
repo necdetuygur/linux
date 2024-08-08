f3ef40708d1b7ffb1fd098d680fcb4e1f8c393b3
Necdet UYGUR <necdet.uygur@gmail.com>
2024-08-08 03:35:04 +0300

    repeat

--- repeat.sh
+++ repeat.sh
@@ -7,5 +7,8 @@ sudo echo ""
 sudo apt update -y
 curl -L -o "Repeat.jar" -C - "https://github.com/repeats/Repeat/releases/download/v5.7.1/Repeat_5_7_1.jar"
 sudo apt install default-jre -y
-echo "java -jar Repeat.jar" > RepeatStart.sh
-sudo chmod +x RepeatStart.sh
+echo "java -jar Repeat.jar" > repeat
+sudo chmod +x repeat
+
+sudo mv Repeat.jar /usr/local/bin/
+sudo mv repeat /usr/local/bin/
