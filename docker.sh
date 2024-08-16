aa8e698dc6412562e10c373decbee753fd7c36d1
Necdet UYGUR <necdet.uygur@gmail.com>
2024-08-16 22:40:57 +0300

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -9,6 +9,13 @@ sudo sh get-docker.sh
 sudo apt install -y uidmap
 dockerd-rootless-setuptool.sh install
 
+sudo echo "sudo systemctl start docker.socket" >> /usr/local/bin/dockerstart
+sudo echo "sudo systemctl start docker" >> /usr/local/bin/dockerstart
+sudo echo "sudo systemctl stop docker.socket" >> /usr/local/bin/dockerstop
+sudo echo "sudo systemctl stop docker" >> /usr/local/bin/dockerstop
+sudo chmod -R 777 /usr/local/bin/dockerstart
+sudo chmod -R 777 /usr/local/bin/dockerstop
+
 # sudo docker run -it --rm -d -p 8080:80 --name web1 nginx:alpine
 # sudo docker ps
 # sudo docker exec -ti web1 sh
