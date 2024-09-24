949b2343ca23b56dbd7da7d09b0a8f55ad3dfacd
Necdet UYGUR <necdet.uygur@gmail.com>
2024-09-24 22:03:36 +0000

    Coolify

--- coolify.sh
+++ coolify.sh
@@ -4,5 +4,4 @@ sudo echo "   | $(date "+%F %H:%M:%S") |"
 sudo echo "   \\*********************/"
 sudo echo ""
 
-sudo su
-curl -fsSL https://cdn.coollabs.io/coolify/install.sh | bash
+curl -fsSL https://cdn.coollabs.io/coolify/install.sh | sudo bash
