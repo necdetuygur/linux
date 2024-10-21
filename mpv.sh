fb0e698e28252d58eb05426012d7054180f7c6ef
Necdet UYGUR <necdet.uygur@gmail.com>
2024-10-21 03:03:01 +0300

    MPV

--- mpv.sh
+++ mpv.sh
@@ -7,12 +7,12 @@ sudo echo ""
 sudo apt install mpv -y
 
 echo "s add speed -0.05" >> ~/.config/mpv/input.conf
-echo "d add speed 0.05" >> ~/.config/mpv/input.conf
 echo "S add speed -0.05" >> ~/.config/mpv/input.conf
+echo "d add speed 0.05" >> ~/.config/mpv/input.conf
 echo "D add speed 0.05" >> ~/.config/mpv/input.conf
 echo "UP add volume 5" >> ~/.config/mpv/input.conf
 echo "DOWN add volume -5" >> ~/.config/mpv/input.conf
 echo "j seek -10" >> ~/.config/mpv/input.conf
-echo "l seek 10" >> ~/.config/mpv/input.conf
 echo "J seek -10" >> ~/.config/mpv/input.conf
+echo "l seek 10" >> ~/.config/mpv/input.conf
 echo "L seek 10" >> ~/.config/mpv/input.conf
