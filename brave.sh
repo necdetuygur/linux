3bbae445ce2814a8bf1f6b1c8e87cacbc529cd9d
Necdet UYGUR <necdet.uygur@gmail.com>
2024-12-23 13:11:44 +0300

    Brave

--- brave.sh
+++ brave.sh
@@ -4,11 +4,13 @@ sudo echo "   | $(date "+%F %H:%M:%S") |"
 sudo echo "   \\*********************/"
 sudo echo ""
 
-sudo apt install -fy curl
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt update
-sudo apt install -fy brave-browser
+curl -fsS https://dl.brave.com/install.sh | sh
+
+# sudo apt install -fy curl
+# sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+# echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+# sudo apt update
+# sudo apt install -fy brave-browser
 
 # sudo apt update
 ## sudo apt install -fy wget
