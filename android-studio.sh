b65edf13e64ea8f3779d1d4dec8eeff86cc20d93
Necdet UYGUR <necdet.uygur@gmail.com>
2025-01-02 00:35:17 +0300

    Android Studio & Java

--- android-studio.sh
+++ android-studio.sh
@@ -7,3 +7,8 @@ sudo echo ""
 sudo add-apt-repository ppa:maarten-fonville/android-studio -y
 sudo apt update -y
 sudo apt install android-studio -y
+
+echo "" >> ~/.bashrc
+echo 'export ANDROID_HOME=~/Android/Sdk' >> ~/.bashrc
+echo 'export PATH=$ANDROID_HOME:$PATH' >> ~/.bashrc
+source ~/.bashrc
---
+++ java.sh
@@ -0,0 +1,12 @@
+sudo echo ""
+sudo echo "   /*********************\\"
+sudo echo "   | $(date "+%F %H:%M:%S") |"
+sudo echo "   \\*********************/"
+sudo echo ""
+
+sudo apt install openjdk-17-jdk -y
+
+echo "" >> ~/.bashrc
+echo 'export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64' >> ~/.bashrc
+echo 'export PATH=$JAVA_HOME/bin:$PATH' >> ~/.bashrc
+source ~/.bashrc
