61975711a7ba2a14a29308dde4ef5e3d916e979b
Necdet UYGUR <necdet.uygur@gmail.com>
2025-10-03 21:37:22 +0300

    Anydesk

--- anydesk.sh
+++ anydesk.sh
@@ -4,7 +4,14 @@ sudo echo "   | $(date "+%F %H:%M:%S") |"
 sudo echo "   \\*********************/"
 sudo echo ""
 
-curl -L -o "anydesk.deb" -C - "https://download.anydesk.com/linux/anydesk_6.3.2-1_amd64.deb"
+sudo apt install libgles-dev libegl-dev -fy
+sudo ln -s /usr/lib/arm-linux-gnueabihf/libGLESv2.so /usr/lib/libbrcmGLESv2.so
+sudo ln -s /usr/lib/arm-linux-gnueabihf/libEGL.so /usr/lib/libbrcmEGL.so
+
+curl -L -o "anydesk.deb" -C - "https://download.anydesk.com/linux/anydesk_7.1.0-1_amd64.deb"
 sudo apt install -fy ./anydesk.deb
 sudo apt install -f
 sudo apt install -fy ./anydesk.deb
+
+echo "Z3FW24WxHF02UIkBYJrG" | sudo anydesk --set-password
+anydesk --get-id
