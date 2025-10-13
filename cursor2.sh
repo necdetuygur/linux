842eca3d748bf5b899b60d1bf80689416b34f804
Necdet UYGUR <necdet.uygur@gmail.com>
2025-10-13 12:26:30 +0300

    Bibata

--- cursor2.sh
+++ cursor2.sh
@@ -7,9 +7,13 @@ sudo echo ""
 sudo apt update -y
 sudo apt install gnome-tweaks -y
 
+curl -L -o "Bibata-Modern-Amber.tar.xz" -C - "https://github.com/ful1e5/Bibata_Cursor/releases/download/v2.0.7/Bibata-Modern-Amber.tar.xz"
 curl -L -o "Bibata-Modern-Classic.tar.xz" -C - "https://github.com/ful1e5/Bibata_Cursor/releases/download/v2.0.7/Bibata-Modern-Classic.tar.xz"
+curl -L -o "Bibata-Modern-Ice.tar.xz" -C - "https://github.com/ful1e5/Bibata_Cursor/releases/download/v2.0.7/Bibata-Modern-Ice.tar.xz"
 
+tar -xvf Bibata-Modern-Amber.tar.xz
 tar -xvf Bibata-Modern-Classic.tar.xz
-rm -rf Bibata-Modern-Classic.tar.xz
+tar -xvf Bibata-Modern-Ice.tar.xz
+rm -rf Bibata-*.tar.xz
 sudo cp -r Bibata-* /usr/share/icons/
-rm -rf Bibata-Modern-Classic
+rm -rf Bibata-*
