7930abdcdceadaaec8123ed5dfa221b29b5b200f
Necdet UYGUR <necdet.uygur@gmail.com>
2025-11-01 21:48:36 +0300

    Coolify

--- coolify.sh
+++ coolify.sh
@@ -6,4 +6,18 @@ sudo echo "   | $(date "+%F %H:%M:%S") |"
 sudo echo "   \\*********************/"
 sudo echo ""
 
-curl -fsSL https://cdn.coollabs.io/coolify/install.sh | sudo bash
+# curl -fsSL https://cdn.coollabs.io/coolify/install.sh | sudo bash
+
+sudo cp /etc/os-release /etc/os-release.bak
+
+sudo cat <<'EOF' | sudo tee /etc/os-release
+PRETTY_NAME="Debian GNU/Linux"
+NAME="Debian"
+ID=debian
+ID_LIKE=debian
+VERSION_ID="11"
+EOF
+
+curl -fsSL https://cdn.coollabs.io/coolify/install.sh | bash
+
+sudo cp /etc/os-release.bak /etc/os-release
